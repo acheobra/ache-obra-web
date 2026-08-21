682b7b0615c04375648929105e54eef7f58e33df
acheobra <acheobra@gmail.com>
2026-08-20 23:43:02 -0300

    Fix sessao de redefinicao de senha, 2 campos e olhos

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1536702800" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3568568398" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
