563b27dc675e22cf3a59cae7255f5e11445f06d1
acheobra <acheobra@gmail.com>
2026-08-26 14:52:43 -0300

    Retirada do auto complete do login do admin

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "502598488" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4258381461" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
