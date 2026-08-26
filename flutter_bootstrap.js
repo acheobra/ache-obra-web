827c71ad6caefccc27577afaa094e009ab0b5366
acheobra <acheobra@gmail.com>
2026-08-26 08:57:15 -0300

    Quinta alteração na tela de usuários

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "430768896" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2662649035" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
