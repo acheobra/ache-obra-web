fd31f3016272cb353488e040034014e90087babd
acheobra <acheobra@gmail.com>
2026-09-02 14:08:27 -0300

    2º Atualização da rotina na sub-aba aba de Notificação de Push Manual

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2113059836" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2545601951" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
